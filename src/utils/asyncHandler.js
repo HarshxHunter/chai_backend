ac2d7a87ec3dcb60756697395fe52e038dd12422
Harsh Singh Kaira <kairaharsh793@gmail.com>
2025-01-24 18:03:17 +0530

    utils code start and discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+// asyncHandler is a higher order function so it takes a function as a parameter and return a function
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}} or (func) => () => {}
+// const asyncHandler = (func) => async () => {} 
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// } 
